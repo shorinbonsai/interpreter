413a60809dd05632a8f06770db73d19be3594671
James Sargant <shorinbonsai@gmail.com>
2023-02-02 22:02:39 -0500

    lexer working

--- token/token.go
+++ token/token.go
@@ -17,6 +17,15 @@ const (
     //operators
     ASSIGN = "="
     PLUS   = "+"
+    MINUS  = "-"
+    BANG   = "!"
+    ASTERISK = "*"
+    SLASH  = "/"
+
+    LT = "<"
+    GT = ">"
+    EQ = "=="
+    NOT_EQ = "!="
 
     //delimeters
     COMMA     = ","
@@ -28,11 +37,21 @@ const (
     //keywords
     FUNCTION = "FUNCTION"
     LET      = "LET"
+    TRUE     = "TRUE"
+    FALSE    = "FALSE"
+    IF       = "IF"
+    ELSE     = "ELSE"
+    RETURN   = "RETURN"
 )
 
 var keywords = map[string]TokenType{
     "fn":  FUNCTION,
     "let": LET,
+    "true": TRUE,
+    "false": FALSE,
+    "if": IF,
+    "else": ELSE,
+    "return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
